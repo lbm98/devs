7a01da0ce5a4ffa5122331c2fa1c2375e3eff337
lbm98 <lars.moons@student.uantwerpen.be>
2022-12-12 12:25:58 +0100

    new

--- examples/port/generator.py
+++ examples/port/generator.py
@@ -1,4 +1,5 @@
-from pypdevs.DEVS import AtomicDEVS
+from pypdevs.minimal import AtomicDEVS
+# from pypdevs.DEVS import AtomicDEVS
 from pypdevs.infinity import INFINITY
 
 from dataclasses import dataclass
@@ -8,20 +9,22 @@ import random
 import numpy as np
 
 # Average amount of vessels arriving at the port on an hourly basis
-NUM_VESSELS_ARRIVING_PER_HOUR = [100, 120, 150, 175, 125, 50, 42, 68, 200, 220, 250, 245, 253, 236, 227, 246, 203, 43,
+NUM_VESSELS_ARRIVING_PER_HOUR = [20, 120, 150, 175, 125, 50, 42, 68, 200, 220, 250, 245, 253, 236, 227, 246, 203, 43,
                                  51, 33, 143, 187, 164, 123]
+
+
 SECONDS_PER_HOUR = 3600
 HOURS_PER_DAY = 24
 
 
 @dataclass
 class GeneratorState:
-    # Remaining time until generation of new event
+    # The remaining time until generation of new event
     remaining: float = 0.0
-    # Current simulation time
+    # The current simulation time
     current_time: float = 0.0
-
-    last_vessel_number_in_current_hour: int = 0
+    # The number of vessels generated
+    vessel_count: int = 0
 
 
 class Generator(AtomicDEVS):
@@ -31,27 +34,17 @@ class Generator(AtomicDEVS):
         self.state = GeneratorState()
 
     def intTransition(self):
-        # Compute the hour, before the time advance
-        old_hour = int(self.state.current_time) // SECONDS_PER_HOUR + 1
-
         # Update simulation time
         self.state.current_time += self.timeAdvance()
 
         # Find the index of the bar chart
-        hour = int(self.state.current_time) // SECONDS_PER_HOUR + 1
-        mod_hour = hour % HOURS_PER_DAY
-
-        if hour != old_hour:
-            self.state.last_vessel_number_in_current_hour = 1
-        else:
-            self.state.last_vessel_number_in_current_hour += 1
+        hour = int(self.state.current_time) // SECONDS_PER_HOUR
 
         # Index the bar chart
-        num_vessels = NUM_VESSELS_ARRIVING_PER_HOUR[mod_hour]
+        num_vessels_per_hour_mean = NUM_VESSELS_ARRIVING_PER_HOUR[hour % HOURS_PER_DAY]
 
         # Compute the Inter-Arrival-Time
-        iat = np.random.gamma(self.state.last_vessel_number_in_current_hour, 1 / num_vessels)
-        iat = iat * (2/num_vessels) * SECONDS_PER_HOUR # rescale
+        iat = np.random.exponential(SECONDS_PER_HOUR/num_vessels_per_hour_mean)
         self.state.remaining = iat
 
         return self.state
@@ -61,7 +54,8 @@ class Generator(AtomicDEVS):
         return self.state.remaining
 
     def outputFnc(self):
-        # Sample vessel type
+        self.state.vessel_count += 1
+        # Choose vessel type
         weights = [vessel.prob for vessel in ALL_VESSELS]
         chosen_vessel_obj = random.choices(ALL_VESSELS, weights=weights)[0]
         return {self.out: chosen_vessel_obj()}
---
+++ examples/port/generator_experiment.py
@@ -0,0 +1,16 @@
+from pypdevs.minimal import Simulator
+# from pypdevs.simulator import Simulator
+from generator import Generator
+
+system = Generator(name="system")
+sim = Simulator(system)
+# simulate 1 hour
+sim.setTerminationTime(3600.0)
+sim.setVerbose(None)
+sim.setClassicDEVS()
+
+sim.simulate()
+
+# should be around 100
+# that is, the first index of the bar chart
+print(f"number of vessels generated: {system.state.vessel_count}")
